f8c6ed93a48a9ad8bada4a97fe59a15cbe985a4e
TheRekZa <txzazalnw@gmail.com>
2025-08-07 10:10:50 +0700

    Update SQLQuery111111.sql

--- SQLQuery111111.sql
+++ SQLQuery111111.sql
@@ -1,4 +1,31 @@
--- 1. ??????????????? 15 ???
+--นายกิติธัช ไชยสัจ 67040249108
+-- 1. สินค้าที่มีราคา 15 บาท
 select * from Products where UnitPrice = 15
--- 2. ????????????????????????????????????? 250
-select * from Products where UnitsInStock < 250
+-- 2. สินค้าที่มีจำนวนคงเหลือในสต๊อกต่ำกว่า 250
+select * from Products where UnitsInStock<250
+-- 3. รหัสสินคา ชื่อสินค้าที่เลิกจำหน่ายแล้ว
+select ProductID, ProductName from Products where Discontinued = 1
+-- 4. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีราคามากกว่า 100
+select ProductID,ProductName,UnitPrice from Products where UnitPrice > 100
+-- 5. รหัสสินค้า และราคาของยางลบ
+select ProductID,UnitPrice from Products where ProductName like '%ยางลบ%'
+-- 6. หมายเลขใบเสร็จ วันที่ และ ราคารวม ของใบเสร็จที่ออกก่อนวันที่ 15 ก.พ.
+select ReceiptID,ReceiptDate,TotalCash from Receipts where ReceiptDate < '2013-02-15'
+-- 7. รหัสสินค้า ชื่อสินค้า ที่มีจำนวนคงเหลือตั้งแต่ 400 ขึ้นไป
+select productID,ProductName from Products where UnitsInStock > 400
+-- 8. รหัสสินค้า ชื่อสินค้า ราคา และ จำนวนคงเหลือ ของแชมพู,แป้งเด็ก,ดินสอ,ยางลบ
+select productID,ProductName,UnitPrice,UnitsInStock from Products where ProductName in ('แชมพู','แป้งเด็ก','ดินสอ','ยางลบ')
+-- 9. รายละเอียดของสินค้าประเภทเครื่องเขียน
+select [Description] from Categories where CategoryName = 'เครื่องเขียน'
+-- 10. รหัสประเภทสินค้า ชื่อประเภท และรายละเอียดของ สินค้าประเภทเครื่องสำอาง
+select CategoryID,CategoryName,[Description] from Categories
+-- 11.คำนำหน้า ชื่อ นามสกุล ของพนักงานที่เป็น Sale Representative
+select Title,FirstName,LastName from Employees
+-- 12. รหัสพนักงาน ชื่อพนักงาน ชื่อผู้ใช้ รหัสผ่าน ของพนักงานทุกคน
+select EmployeeID,Title + FirstName+SPACE(1)+LastName EmployeeName,USERNAME,[Password] from Employees
+-- 13. ชื่อผู้ใช้ และรหัสผ่านของพนักงานที่ชื่อก้องนิรันดร์
+select UserName,Password from Employees where FirstName = 'ก้องนิรันดร์'
+-- 14. รหัสพนักงานที่ออกใบเสร็จหมายเลข 3
+select EmployeeID from Receipts where ReceiptID = 3
+-- 15. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีรหัสประเภท 2, 4
+select productID,ProductName unitprice from Products where CategoryID in(2,4)
